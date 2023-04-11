87be9354d86f9b5b4d7828e6aed441182b157f25
AnthonyNet <zejda.antonin@gmail.com>
2023-04-11 17:56:06 +0200

    preview

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -6,6 +6,9 @@ import {Exercise_1_App, Exercise_1_Answer, Exercise_1_Section} from './component
 
 const Playground = () => {
 const [showAnswer, setShowAnswer] = useState(false)
+const handleClick = () => {
+setShowAnswer((showAnswer) => !showAnswer);
+}
 
   return (
 		<section className="grid items-center align-center h-screen w-screen">
@@ -14,7 +17,7 @@ const [showAnswer, setShowAnswer] = useState(false)
 				preview={Exercise_1_Section}
 			/>
 			<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-			 onClick={() => setShowAnswer((showAnswer) => !showAnswer)}>
+			 onClick={handleClick}>
 				Ukaž odpověď
 			</button>
 		</section>
