1ef6a8e34aebeffdb3ae873f3f28c256aacfd7f9
AnthonyNet <zejda.antonin@gmail.com>
2023-05-13 10:10:29 +0200

    formatter only

--- src/utils/classNames.tsx
+++ src/utils/classNames.tsx
@@ -14,28 +14,28 @@ import { THEME_PREFIX } from "../styles";
 const ClassNamesContext = createContext<Record<string, string>>({});
 
 export const ClassNamesProvider: React.FC<
-  React.PropsWithChildren<{ classes?: Record<string, string> }>
+	React.PropsWithChildren<{ classes?: Record<string, string> }>
 > = ({ children, classes }) => (
-  <ClassNamesContext.Provider value={classes || {}}>
-    {children}
-  </ClassNamesContext.Provider>
+	<ClassNamesContext.Provider value={classes || {}}>
+		{children}
+	</ClassNamesContext.Provider>
 );
 
 export const useClassNames = () => {
-  const contextClassNames = useContext(ClassNamesContext);
+	const contextClassNames = useContext(ClassNamesContext);
 
-  return function sandpackClassNames(
-    customClassName: string,
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-    allClassNames: any[] = []
-  ): string {
-    const custom = `${THEME_PREFIX}-${customClassName}`;
+	return function sandpackClassNames(
+		customClassName: string,
+		/* eslint-disable @typescript-eslint/no-explicit-any */
+		allClassNames: any[] = []
+	): string {
+		const custom = `${THEME_PREFIX}-${customClassName}`;
 
-    return joinClassNames(...allClassNames, custom, contextClassNames[custom]);
-  };
+		return joinClassNames(...allClassNames, custom, contextClassNames[custom]);
+	};
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const joinClassNames = (...args: any[]): string => {
-  return args.filter(Boolean).join(" ");
+	return args.filter(Boolean).join(" ");
 };
