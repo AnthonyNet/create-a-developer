d0d07e5862c7bc004dd8d983a389a454658932ad
Petr Glaser <syreanis+1@gmail.com>
2023-05-29 22:39:28 +0200

    fix(sandpack): show / hide answer file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
+import { Inter } from "next/font/google";
 
-import { Inter } from 'next/font/google'
+import Playground from "../components/NaucMe__SandPack";
+import {
+  Exercise_1_App,
+  exercise,
+  exercise_Answer,
+} from "@/exercises/Exercise_1_Data";
 
-import Playground from '../components/NaucMe__SandPack'
-import { Exercise_1_App, exercise, exercise_Answer} from '@/exercises/Exercise_1_Data';
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-	<section className='flex flex-col items-center justify-center w-screen h-screen'>
-		<Playground app={Exercise_1_App} exercise={exercise} answer={exercise_Answer}/>
- </section>
-  )
+    <section className="flex flex-col items-center justify-center w-screen h-screen">
+      <Playground
+        app={Exercise_1_App("filter")}
+        exercise={exercise}
+        answer={exercise_Answer}
+      />
+    </section>
+  );
 }
