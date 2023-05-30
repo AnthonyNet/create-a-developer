950d4f81f80aa9bf8e0c353e4dafb846da48787a
AnthonyNet <zejda.antonin@gmail.com>
2023-05-30 19:23:04 +0200

    feat(Naucme_Sandpack, Sandpack_Original, Exercise_1_Data.tsx): save user input state from pegak
    after showing the correct answer, the values from the user are still

--- src/components/NaucMe__SandPack.tsx
+++ src/components/NaucMe__SandPack.tsx
@@ -6,73 +6,59 @@ import { amethyst } from "@codesandbox/sandpack-themes";
 import { preview_Css } from "../exercises/preview_Css";
 import { useSandpack } from "@codesandbox/sandpack-react";
 
-
 interface Props {
-	app: string;
-	exercise: string
-	answer: string;
+  app: string;
+  exercise: string;
+  answer: string;
 }
 
 const Playground = ({ app, exercise, answer }: Props) => {
-	const [showAnswer, setShowAnswer] = useState<boolean>(false);
-	const [exerciseSave, setExerciseSave] = useState<string>(exercise);
-
-	const handleClick = () => {
-		setShowAnswer((showAnswer) => !showAnswer);
-	};
-
-	const Setup_Props: CustomProps = {
-		customProps: {
-			showOpenInCodeSandbox: false,
-		},
-		options: {
-			resizablePanels: true,
-			editorHeight: "99.75vh",
-			showConsole: true,
-			showConsoleButton: true,
-			showLineNumbers: true,
-			editorWidthPercentage: 50,
-			closableTabs: true,
-			showTabs: true,
-			/*visibleFiles: ["/filter.js", "/style.css"],
+  const Setup_Props: CustomProps = {
+    customProps: {
+      showOpenInCodeSandbox: false,
+    },
+    options: {
+      resizablePanels: true,
+      editorHeight: "99.75vh",
+      showConsole: true,
+      showConsoleButton: true,
+      showLineNumbers: true,
+      editorWidthPercentage: 50,
+      closableTabs: true,
+      showTabs: true,
+      /*visibleFiles: ["/filter.js", "/style.css"],
 			activeFile: "/filter.js",*/
-			showInlineErrors: true, // What is this doing?
-			//externalResources: ["https://cdn.tailwindcss.com"] eRRoRs
-		},
-		files: {
-			"/App.js": {
-				code: app,
-				//readOnly: true,
-				hidden: true,
-			},
+      showInlineErrors: true, // What is this doing?
+      //externalResources: ["https://cdn.tailwindcss.com"] eRRoRs
+    },
+    files: {
+      "/App.js": {
+        code: app,
+        //readOnly: true,
+        hidden: true,
+      },
 
-			"/filter.js": {
-				code: showAnswer ? answer : exerciseSave,
-				active: true,
-			},
+      "/filter.js": {
+        code: exercise,
+        active: true,
+      },
 
+      "/filterAnswer.js": {
+        code: answer,
+        hidden: true,
+      },
 
-			"/style.css": {
-				code: preview_Css,
-				//active: true,
-				hidden: true,
-			},
-		},
-		template: "react",
-		theme: amethyst,
-	};
+      "/style.css": {
+        code: preview_Css,
+        //active: true,
+        hidden: true,
+      },
+    },
+    template: "react",
+    theme: amethyst,
+  };
 
-	return (
-		<section className="w-screen h-screen">
-			<div className="sandP_Btn w-[240px] h-[40px]  absolute z-10 right-0 md:left-0 md:bottom-0 my-1  flex items-start">
-				<button onClick={handleClick}>Ukaž odpověď</button>
-				<button onClick={() => setShowAnswer(false)}>Restart</button>
-			</div>
-			<Sandpack {...Setup_Props} theme={amethyst} />
-
-
-		</section>
-	);
+  return <Sandpack {...Setup_Props} theme={amethyst} />;
 };
 
 export default Playground;
